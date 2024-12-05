5c26989d3e58fe5c170ed7d7adc66095919a5089
Henk Kampman <henk.kampman@secondmove.com>
2024-12-05 13:15:35 +0100

    Initial Commit

---
+++ ValidatePostalcode/ValidatePostalcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidatePostalcodeApp.swift
+//  ValidatePostalcode
+//
+//  Created by Henk on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ValidatePostalcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ValidatePostalcodeTests/ValidatePostalcodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValidatePostalcodeTests.swift
+//  ValidatePostalcodeTests
+//
+//  Created by Henk on 05/12/2024.
+//
+
+import Testing
+@testable import ValidatePostalcode
+
+struct ValidatePostalcodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
